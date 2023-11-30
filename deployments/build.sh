73287929c06e0ae2c4bb40737773f6c7f75b47cd
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2023-11-30 23:15:06 +0700

    dockerfile copy only needed files

---
+++ deployments/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+imageName="backend"
+
+timestamp=$(date +%Y%m%d%H%M%S)
+
+tag=$imageName:$timestamp
+# latest=$imageName:latest
+
+dir="$(dirname "$(realpath "$0")")"
+
+# # build
+docker build -t "$tag" -f "$dir"/Dockerfile . --progress=plain --no-cache
+
+# # echo latest tag
+# echo "$tag"
