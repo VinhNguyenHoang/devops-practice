6bf9d722305cf6bcfcd4321e9f2ddc1b4dcff9b8
Vinh Nguyen <vinh.nguyen@treehouse.finance>
2024-04-04 15:40:19 +0700

    allow delete images

--- deployments/kind-with-registry.sh
+++ deployments/kind-with-registry.sh
@@ -7,6 +7,7 @@ reg_port='5001'
 if [ "$(docker inspect -f '{{.State.Running}}' "${reg_name}" 2>/dev/null || true)" != 'true' ]; then
   docker run \
     -d --restart=always -p "127.0.0.1:${reg_port}:5000" --name "${reg_name}" \
+    -e REGISTRY_STORAGE_DELETE_ENABLED=true \
     registry:2
 fi
 
