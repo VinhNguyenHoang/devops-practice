ea8937b2dbf1ca6f1b3ee3415a772a134d62849c
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2024-06-13 00:05:37 +0700

    add istio stacks

--- deployments/build_go.sh
+++ deployments/build_go.sh
@@ -17,6 +17,7 @@ buildonly () {
 
     dir="$(dirname "$(realpath "$0")")"
 
+    # TODO: use timestamp as tag
     IMAGE_FULL_PATH=$REGISTRY_HOST/$REGISTRY_REPO/$IMAGE_NAME":latest"
 
     docker build -t $IMAGE_FULL_PATH -f "$dir/"$DOCKERFILE_PATH . --no-cache
