3125829a232f5c0557310e37f659136bfb4aaa77
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2024-06-14 11:50:43 +0700

    remove local img before build

--- deployments/build_go.sh
+++ deployments/build_go.sh
@@ -2,6 +2,16 @@
 
 action="$1"
 
+DOCKERFILE_PATH="multistage.Dockerfile"
+IMAGE_NAME="backend"
+REGISTRY_HOST="localhost:5001"
+REGISTRY_REPO="personal"
+
+dir="$(dirname "$(realpath "$0")")"
+
+# TODO: use timestamp as tag
+IMAGE_FULL_PATH=$REGISTRY_HOST/$REGISTRY_REPO/$IMAGE_NAME":latest"
+
 buildandpush () {
     buildonly
     echo "Pushing to local registry..."
@@ -9,17 +19,12 @@ buildandpush () {
 }
 
 buildonly () {
+    if docker images --format "{{.Repository}}:{{.Tag}}" | grep -q $IMAGE_FULL_PATH; then
+        echo "Deleting $IMAGE_FULL_PATH"
+        docker rmi $IMAGE_FULL_PATH
+    fi
+
     echo "Building image..."
-    DOCKERFILE_PATH="multistage.Dockerfile"
-    IMAGE_NAME="backend"
-    REGISTRY_HOST="localhost:5001"
-    REGISTRY_REPO="personal"
-
-    dir="$(dirname "$(realpath "$0")")"
-
-    # TODO: use timestamp as tag
-    IMAGE_FULL_PATH=$REGISTRY_HOST/$REGISTRY_REPO/$IMAGE_NAME":latest"
-
     docker build -t $IMAGE_FULL_PATH -f "$dir/"$DOCKERFILE_PATH . --no-cache
 }
 
