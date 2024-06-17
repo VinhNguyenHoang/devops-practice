04f8cb668b2a2c7ac30668a05c61a21e8d1f69de
VinhNguyenHoang <vinhnguyenhoang96@gmail.com>
2024-06-17 10:34:56 +0700

    update otel tracing between svc

--- internal/jerry/server.go
+++ internal/jerry/server.go
@@ -1,13 +1,82 @@
 package jerry
 
 import (
-	"cs/internal/jerry/handler"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 )
+
+var tracer = otel.Tracer("test-tracer")
 
 func ExportEndpoints() map[string]func(c *gin.Context) {
 	return map[string]func(c *gin.Context){
-		"/api": handler.HandleGETRequest,
+		"/hello": HandleRequest,
+	}
+}
+
+func HandleRequest(c *gin.Context) {
+	log.Println("Received request")
+
+	reqCtx := c.Request.Context()
+	span := trace.SpanFromContext(reqCtx)
+	defer span.End()
+
+	// call to Spike
+	spikeURL := os.Getenv("SPIKE_URL")
+	if spikeURL == "" {
+		c.AbortWithError(http.StatusInternalServerError, fmt.Errorf("Missing Spike url"))
+		return
+	}
+
+	c.Request.Header.Set("Content-Type", "application/json")
+	req, err := http.NewRequest("GET", spikeURL+"/world", nil)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	otel.GetTextMapPropagator().Inject(reqCtx, propagation.HeaderCarrier(req.Header))
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+	resBody, err := io.ReadAll(res.Body)
+	if err != nil {
+		c.AbortWithError(http.StatusInternalServerError, err)
+		return
+	}
+
+	log.Println("Header", req.Header)
+
+	replyFromSpike := string(resBody)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "reply from server Jerry:" + replyFromSpike,
+	})
+}
+
+func propagateHeaders(a *http.Request, b *http.Request) {
+	headers := []string{
+		"portal",
+		"device",
+		"user",
+		"travel",
+		"x-request-id",
+		"x-b3-traceid",
+		"x-b3-spanid",
+		"x-b3-parentspanid",
+		"x-b3-sampled",
+		"x-b3-flags",
+		"x-ot-span-context",
+	}
+	for _, header := range headers {
+		b.Header.Add(header, a.Header.Get(header))
 	}
 }
